f31131fbb4a14a076beb26cbfe4fec15ff9e4435
AnasAA412 <Anasanchu4430@gmail.com>
2025-04-09 19:59:36 +0530

    trying to deploy on vercel

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ const PORT = process.env.PORT || 5005;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.client_domain,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
 
     allowedHeaders: [
